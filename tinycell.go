1cf4421b929c2f92981114f52a9690cb2229dbe6
bhivam <shivamkajaria@gmail.com>
2024-02-02 13:26:47 -0500

    sync

--- tinycell.go
+++ tinycell.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "os"
+    "strconv"
 )
 
 type op int
+type cell_type int
 
 const (
     ADD op = iota
@@ -14,18 +16,27 @@ const (
     DIV op = iota
 )
 
+const (
+    equation cell_type = iota
+    literal cell_type = iota
+)
+
 type eq struct {
     operation op
-    operand1 *cell
-    operand2 *cell
+    value1 int
+    value2 int
+    col1 int
+    row1 int
+    col2 int
+    row2 int
 }
 
 type cell struct {
     col int
     row int
-    lexeme string
-    value float32
+    value float64
     equation eq
+    ctype cell_type
 }
 
 func check(e error) {
@@ -39,32 +50,58 @@ func parse(file []byte) ([][]cell) {
     grid := [][]cell{{}}
     cur_row := 0
     cur_col := 0
-
     curr_word := ""
+    var err error
+    
+    new_cell := cell{row: cur_row, col: cur_col, ctype: literal}
 
     for i := 0; i < len(file); i++ {
         switch file[i] {
+        case '=':
+            if curr_word != "" {
+                curr_word += string(file[i])
+            } else {
+                new_cell.ctype = equation
+            }
+        case '+': // add in other signs later
+            if new_cell.ctype != equation {
+                curr_word += string(file[i])
+            } else {
+                // curr_word, here, is an int literal or a cell
+                // make a function to fgure it out
+            }
         case ',':
-            new_cell := cell{
-                col: cur_col,
-                row: cur_row,
-                lexeme: curr_word,
+            if new_cell.ctype == equation {
+                // curr_word, here, is an int literal or a cell
+                // make a function to fgure it out
+            } else {
+                fmt.Printf("%s", curr_word)
+                new_cell.value, err = strconv.ParseFloat(curr_word, 64)
+                check(err)
             }
             grid[cur_row] = append(grid[cur_row], new_cell)
             fmt.Println("comma")
             curr_word = ""
             cur_col += 1
+            new_cell = cell{row: cur_row, col: cur_col}
         case '\n':
-            new_cell := cell{
-                col: cur_col,
-                row: cur_row,
-                lexeme: curr_word,
+            // We want to detect when the type is different from this
+            if new_cell.ctype == equation {
+                // curr_word, here, is an int literal or a cell
+                // make a function to fgure it out
+            } else {
+                fmt.Printf("%s", curr_word)
+                new_cell.value, err = strconv.ParseFloat(curr_word, 64)
+                check(err)
             }
             grid[cur_row] = append(grid[cur_row], new_cell)
             fmt.Println("newline")
             curr_word = ""
             cur_row += 1
+            cur_col = 0;
             grid = append(grid, []cell{})
+        case ' ':
+            continue
         default:
             curr_word += string(file[i])
         }
@@ -81,7 +118,7 @@ func main() {
 
     for i := 0; i < len(grid); i++ {
         for j := 0; j < len(grid[i]); j++ {
-            fmt.Printf("%s,", grid[i][j].lexeme)
+            fmt.Printf("%f,", grid[i][j].value)
         }
         fmt.Println()
     }
